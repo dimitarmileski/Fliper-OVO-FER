2bb1822912b5161f2c8f771867166ca23ff9174a
dimitarmileski <dime.mileski@hotmail.com>
2020-04-28 15:43:46 +0200

    Collision detection

--- Fliper/Assets/BallCollitions.cs
+++ Fliper/Assets/BallCollitions.cs
@@ -1,10 +1,38 @@
 ﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallCollitions : MonoBehaviour
 {
+    public int score = 0;
+
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Hit with something!");
+
+        if (collision.collider.tag == "CylinderPoint")
+        {
+            score += 10;
+            Debug.Log("Score: " + score);
+        }
+
+        else if (collision.collider.tag == "CapsulePoint")
+        {
+            score += 20;
+            Debug.Log("Score: " + score);
+        }
+
+        else if (collision.collider.tag == "CubePoint")
+        {
+            score += 50;
+            Debug.Log("Score: " + score);
+        }
+        else if (collision.collider.tag == "GameOverPlate")
+        {
+            score = 0;
+            Debug.Log("Score: " + score);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      
+        }
+
     }
 }
