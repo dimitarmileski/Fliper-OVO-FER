4ee64aa1ac8da815223d88942444b8b27e380d7e
dimitarmileski <dime.mileski@hotmail.com>
2020-04-28 18:56:59 +0200

    Table points scheduled

--- Fliper/Assets/BallCollitions.cs
+++ Fliper/Assets/BallCollitions.cs
@@ -1,38 +1,39 @@
 ﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BallCollitions : MonoBehaviour
 {
     public int score = 0;
-
+    public TMP_Text scoreText;
+    public Rigidbody ball;
     private void OnCollisionEnter(Collision collision)
     {
 
         if (collision.collider.tag == "CylinderPoint")
         {
             score += 10;
-            Debug.Log("Score: " + score);
+            ball.velocity *=1;
         }
 
         else if (collision.collider.tag == "CapsulePoint")
         {
             score += 20;
-            Debug.Log("Score: " + score);
+            ball.velocity *= 3;
         }
 
         else if (collision.collider.tag == "CubePoint")
         {
             score += 50;
-            Debug.Log("Score: " + score);
+            ball.velocity *= 3;
         }
         else if (collision.collider.tag == "GameOverPlate")
         {
             score = 0;
-            Debug.Log("Score: " + score);
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-      
         }
-
+        scoreText.text = score.ToString();
+        
     }
 }
